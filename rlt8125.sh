64c8a91cd5223ee992fc422f675b70287a1661ad
Van <27133369+tubaxiaosiji@users.noreply.github.com>
2020-05-04 22:04:33 +0800

    Update rlt8125.sh

--- rlt8125.sh
+++ rlt8125.sh
@@ -1,15 +1,35 @@
 # /bin/bash
-# This script is auto complie RLT8125 driver for PVE
+# This script is automaticlly complie RLT8125 driver for PVE
 # date 2020/4/25 22:35 UTC +8:00
+# Check your PVE version
+PveVersion=`uname -r`
+sed xx $PveVersion xxxx
 
-# author: tubaxiaosiji
-main_version=akw 'xx'
-YOUR_PVE_KERNEL_SUB_VERSION=`uname -r`
-if [find $(YOUR_PVE_KERNEL_SUB_VERSION)!='PVE']
-	echo 'Your system is not PVE, please check your system'
-	exit 2
-fi
-apt-get install dkms build-essential pve-headers-$(YOUR_PVE_KERNEL_SUB_VERSION) pve-headers-$(main_version)
-tar -xvf ./rtdrivers/cn/nic1/r8125-9.003.04.tar.bz2
+if PveVersion=5.0{
+	add PVE 5.0 no subcript to apt source.list
+}
+
+if PveVersion=6.0{
+	add PVE 6.0 no subcript to apt source.list
+}
+
+if PveVersion=4.0{
+	add PVE 4.0 no subcript to apt source.list
+}
+
+apt-update
+
+apt-get install ${PveVersion_kernel_Headers}
+
+tar -xvf $PWD/r8125-9.003.04.tar
 cd r8125-9.003.04
-chmod a+x ./
+chmod a+x xx.sh
+./xx.sh
+# check loaded kernel modules exist 'rtl8152' keyword
+lsmod | grep '8152'
+if [ is ture]{
+	print('RLT8125 driver has been Installed!')
+}
+else{
+	print('Please tay again.')
+}
