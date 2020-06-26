6afae3bd3c8224e601102ced962f8421184cd471
Van <27133369+tubaxiaosiji@users.noreply.github.com>
2020-06-26 22:32:36 +0800

    Update rtl8125_install.sh

--- rtl8125_install.sh
+++ rtl8125_install.sh
@@ -2,19 +2,24 @@
 # This script is automaticlly complie RTL8125 2.5G ethernet card driver for PVE
 # date 2020/4/25 22:35 UTC +8:00
 
-# Check rtl8125 kernel modules exist it 【检查驱动是否加载成功】
+# Check rtl8125 kernel modules exist it 【检查驱动是否加载】
 check=`lsmod | grep rtl8152`
 if [ "$check" != "" ]; then
-	echo 'RTL8125 driver has been Installed! [恭喜！网卡驱动已加载！]'
+	echo 'RTL8125 driver has been Installed! [恭喜！当前网卡驱动已加载！]'
 	exit 0
 fi
 # Get Proxmox VE kernel version 【获取PVE内核版本】
 
 PVE_kernel_version=`uname -r`
+uname -r > /tmp/PVE_kernel_version.log
+check=`grep "pve" /tmp/PVE_kernel_version.log`
 
-if [ 'pve' not in $PVE_kernel_version ]; then
-	echo 'Sorry, your system is not supported it, please run it on Proxmox VE 3.0-6.0 [抱歉，您的系统不被支持，请在 PVE 3.0-6.0 下使用。]'
-	exit -1
+if [ "${check}" != "" ]; then
+        echo "Check is OK!\n [检测通过........]\n"
+        rm /tmp/PVE_kernel_version.log
+else
+        echo "Sorry, your system is not supported it, please run it on Proxmox VE 3.0-6.0. \n[抱歉，您的系统不被支持，请在 PVE 3.0-6.0 下使用。]\n"
+        exit -1
 fi
 
 
@@ -22,9 +27,8 @@ pve_kernel_headers_version=pve-headers-${PVE_kernel_version}
 
 # Get PVE Full version 【获取当前PVE完整版本】
 PVE_Full_version=`pveversion`
-
 # Get PVE Main version 【获取当前PVE主版本，添加软件仓库源会用到】
-PVE_main_version=`awk xx PVE_Full_version`
+${PVE_full_version:12:1}
 
 # Add no subcript source 【添加非订阅用户源】
 # NOT recommended for production use 【不建议生产环境中使用】
