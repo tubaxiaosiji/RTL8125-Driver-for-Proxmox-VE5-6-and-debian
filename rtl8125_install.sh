9b684d18c777174a8e712acb688fa1f7687ded8b
Van <27133369+tubaxiaosiji@users.noreply.github.com>
2020-06-26 22:59:01 +0800

    Update rtl8125_install.sh

--- rtl8125_install.sh
+++ rtl8125_install.sh
@@ -15,10 +15,10 @@ uname -r > /tmp/PVE_kernel_version.log
 check=`grep "pve" /tmp/PVE_kernel_version.log`
 
 if [ "${check}" != "" ]; then
-        echo "Check is OK! [检测通过........]"
+        echo "Kernel version is matched! [内核版本检测通过........]"
         rm /tmp/PVE_kernel_version.log
 else
-        echo "Sorry, your system is not supported it, please run it on Proxmox VE 3.0-6.0. [抱歉，您的系统不被支持，请在 PVE 3.0-6.0 下使用。]"
+        echo "Sorry, your Kernel version is not supported it, please run it on Proxmox VE [抱歉，您的内核不被支持，请在 PVE 下使用。]"
         exit -1
 fi
 
@@ -28,23 +28,24 @@ pve_kernel_headers_version=pve-headers-${PVE_kernel_version}
 # Get PVE Full version 【获取当前PVE完整版本】
 PVE_Full_version=`pveversion`
 # Get PVE Main version 【获取当前PVE主版本，添加软件仓库源会用到】
-PVE_main_version=${PVE_full_version:12:1}
+PVE_Main_version=`echo ${PVE_Full_version:12:1}`
+
 
 # Add no subcript source 【添加非订阅用户源】
 # NOT recommended for production use 【不建议生产环境中使用】
 # PVE pve-no-subscription repository provided by proxmox.com 【非订阅用户软件仓库由proxmox.com提供】
 
 
-if [ "$PVE_main_version" == 6 ]; then
+if [ "$PVE_Main_version" == 6 ]; then
 	# add PVE 6.0 no subcript to apt source.list
 	deb http://download.proxmox.com/debian/pve buster pve-no-subscription
-elif [ "$PVE_main_version" == 5 ]; then
+elif [ "$PVE_Main_version" == 5 ]; then
 	# add PVE 5.0 no subcript to apt source.list
 	deb http://download.proxmox.com/debian stretch pve-no-subscription
-elif [ "$PVE_main_version" == 4 ]; then
+elif [ "$PVE_Main_version" == 4 ]; then
 	# add PVE 4.0 no subcript to apt source.list
 	deb http://download.proxmox.com/debian jessie pve-no-subscription
-elif [ "$PVE_main_version" == 3 ]; then
+elif [ "$PVE_Main_version" == 3 ]; then
 	# add PVE 3.0 no subcript to apt source.list
 	deb http://download.proxmox.com/debian wheezy pve-no-subscription
 else 
