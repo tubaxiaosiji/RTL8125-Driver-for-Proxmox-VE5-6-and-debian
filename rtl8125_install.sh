b21b444ef2ea6bfccb218a06dbba1b32b5123bd5
Van <27133369+tubaxiaosiji@users.noreply.github.com>
2020-06-27 01:28:24 +0800

    Update rtl8125_install.sh

--- rtl8125_install.sh
+++ rtl8125_install.sh
@@ -67,8 +67,9 @@ fi
 
 
 apt-get update
+apt-get -y install ${pve_kernel_headers_version}
 # Install dependent packages 【安装依赖包】
-apt-get -y install ${pve_kernel_headers_version} dkms build-essential make gcc linux-compiler-gcc-8-x86
+apt-get -y install dkms build-essential make gcc linux-compiler-gcc-8-x86
 
 
 tar -xvf $PWD/r8125-9.003.04.tar
